0bc36da544a96282e8ba40d6eba5c11c4010d14c
hamza <traorehamza407@gmail.com>
2025-05-22 16:58:25 +0200

    Ajout du bundle d'authentification JWT et configuration associée

--- src/Entity/Doctor.php
+++ src/Entity/Doctor.php
@@ -4,10 +4,16 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DoctorRepository;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DoctorRepository::class)]
 #[ApiResource]
+
+#[ApiResource]
+#[ApiFilter(SearchFilter::class, properties: ['city' => 'partial', 'speciality' => 'partial'])]
 class Doctor
 {
     #[ORM\Id]
@@ -16,9 +22,13 @@ class Doctor
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 50)]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 50)]
     private ?string $lastname = null;
 
     #[ORM\Column(length: 50)]
@@ -28,12 +38,30 @@ class Doctor
     private ?string $address = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 50)]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z\s]+$/',
+        message: 'Le city doit contenir uniquement des lettres et des espaces'
+    )]
     private ?string $city = null;
 
     #[ORM\Column(length: 5)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 5)]
+    #[Assert\Regex(
+        pattern: '/^\d{5}$/',
+        message: 'le code postal doit contenir 5 chiffres.'
+    )]
     private ?string $zip = null;
 
     #[ORM\Column(length: 12, nullable: true)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 12)]
+    #[Assert\Regex(
+        pattern: '/^\d{10,12}$/',
+        message: 'Le numéro de téléphone doit contenir entre 10 et 12 chiffres.'
+    )]
     private ?string $phone = null;
 
     public function getId(): ?int
