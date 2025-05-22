34f7b3166589e82e9bcdc8f6116e9b2e755c17ac
hamza <traorehamza407@gmail.com>
2025-05-22 15:13:41 +0200

    Ajout de la migration pour créer la table doctor avec ses colonnes associées

---
+++ migrations/Version20250522120021.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250522120021 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE doctor (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, speciality VARCHAR(50) NOT NULL, address VARCHAR(255) NOT NULL, city VARCHAR(50) NOT NULL, zip VARCHAR(5) NOT NULL, phone VARCHAR(12) DEFAULT NULL)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE doctor
+        SQL);
+    }
+}
